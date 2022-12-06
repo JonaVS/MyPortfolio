3f7fb3d197d3fa9b7a12527939942c597b5c2b9a
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-12-06 14:47:21 -0600

    Refactor Use styled components at About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,16 @@
 import React from "react"
 import Layout from "../components/shared/Layout/Layout"
 import SEOComponent from "../components/shared/SEO/seo"
-import Container from "../components/shared/Container/Container"
+import { AboutContainer } from "../styles/about.styles"
 import AboutSection from "../components/AboutSection/AboutSection"
-import * as styles from "../styles/about.module.css"
 
 export default function About() {
   return (
     <Layout>
       <SEOComponent title="About" description="About JVS" slug="/about" />
-      <Container className={styles.container}>
+      <AboutContainer>
         <AboutSection />
-      </Container>
+      </AboutContainer>
     </Layout>
   )
 }
---
+++ src/styles/about.styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+import Container from "../components/shared/Container/Container"
+
+export const AboutContainer = styled(Container)`
+  margin: 50px auto 50px auto;
+  padding: 0px 0px 20px 0px;
+`
