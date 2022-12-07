4b89e464eff44f86a95f4bee8f5c8475a70e96c4
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-12-07 15:06:50 -0600

    Refactor: Move Container from Projects page to projects Section

---
+++ src/components/ProjectsSection/projectsSection.styles.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+import Container from "../shared/Container/Container"
+
+export const ProjectsContainer = styled(Container)`
+  margin: 50px auto 50px auto;
+  background-color: transparent;
+  box-shadow: none;
+`
