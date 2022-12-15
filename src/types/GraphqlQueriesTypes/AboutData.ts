89c658f1b44382c0a951225c14bc598528a1cf2d
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-12-14 18:41:25 -0600

    Feat: Use Contentful CMS data at Knowledge section

--- src/types/GraphqlQueriesTypes/AboutData.ts
+++ src/types/GraphqlQueriesTypes/AboutData.ts
@@ -2,7 +2,17 @@ export type AboutData = {
   allContentfulAboutMe: { nodes: AboutNode[] }
 }
 
+export type KnowledgeData = {
+  allContentfulTechnologie: { nodes: TechNode[] }
+}
+
 type AboutNode = {
   node_locale: string
   about: { about: string }
+}
+
+export type TechNode = {
+  id?: string
+  name: string
+  svg: { svg: string }
 }
